b7138bfd06531ec2ea5cee7e465b9e950b8c4cc7
Breno Panzolini <bpanzolini@gmail.com>
2017-07-24 12:10:31 -0300

    Refactor client/main.js

--- client/main.js
+++ client/main.js
@@ -1,9 +1,1 @@
-import { Meteor } from 'meteor/meteor'
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const App = () => <h1>Hello World!</h1>;
-
-Meteor.startup(() => {
-  ReactDOM.render(<App />, document.getElementById('render-target'));
-})
+import '../imports/startup/client';
